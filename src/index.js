bf2cd9134f82cd08e404205301f3c30ddeb3d7f9
Julien Barret <ibarretj@gmail.com>
2017-05-17 11:18:57 +0200

    Apollo basics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import App from './layout/app/App.jsx';
 import './index.css';
 
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj2qk762oejez0103tw2j0a92'}),
+})
+
+
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 ), document.getElementById('root'));
